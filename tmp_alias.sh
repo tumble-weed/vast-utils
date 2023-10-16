378bd6d082bde569ed1eb074ebd67cd4487a5c6a
Aniket Singh <aniketsinghresearch@gmail.com>
2023-10-16 07:15:17 +0000

    ssh alias

--- tmp_alias.sh
+++ tmp_alias.sh
@@ -1,1 +1,4 @@
 alias runsess="cd $ELP; CUDA_VISIBLE_DEVICES=, python -m ipdb -c c examples/attribution_benchmark.py --method extremal_perturbation_with_sess_scale_and_crop --start 0 --end 5000 --continue_ --arch vgg16 --dataset voc_2007"
+alias s115="ssh vast-115"
+alias s117="ssh vast-117"
+alias s119="ssh vast-119"
