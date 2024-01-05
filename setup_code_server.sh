c20f76d77ea1d31bdb02e52df7af21ab3dfa0c24
Aniket Singh <aniketsinghresearch@gmail.com>
2024-01-05 06:32:03 +0000

    fixed code server

--- setup_code_server.sh
+++ setup_code_server.sh
@@ -1,1 +1,4 @@
-/root/.config/code-server/config.yaml 
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+cp $DIR/code_server_config.yaml /root/.config/code-server/config.yaml 
