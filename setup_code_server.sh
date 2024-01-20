4688068fd341f63d27b28527adfb3c035323a6b3
Aniket Singh <aniketsinghresearch@gmail.com>
2024-01-20 11:04:28 +0000

    download code server

--- setup_code_server.sh
+++ setup_code_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+curl -fsSL https://code-server.dev/install.sh | sh
+mkdir -p /root/.config/code-server/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 cp $DIR/code_server_config.yaml /root/.config/code-server/config.yaml 
