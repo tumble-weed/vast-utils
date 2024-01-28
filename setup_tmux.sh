12bde29c97d8cc34b00f4eef159f7d3c6f488c6c
Aniket Singh <aniketsinghresearch@gmail.com>
2024-01-28 12:23:45 +0000

    tmux

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -7,11 +7,11 @@ _tmux_autocomplete_sessions() {
 complete -F _tmux_autocomplete_sessions tmux
 
 tma() {
-    local session_name
+    local session_name="$1"
     local tmux_cmd
 
     # Get the session name using tab completion
-    read -e -i "$1" -p "Enter Tmux session name: " session_name
+    #read -e -i "$1" -p "Enter Tmux session name: " session_name
 
     # Build the tmux command
     tmux_cmd="tmux a -t $session_name"
