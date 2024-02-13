9a513d8a3c51e39ad9e75267fe413b2730596e33
Aniket Singh <aniketsinghresearch@gmail.com>
2024-02-13 07:12:46 +0000

    nvim

--- setup_vim.sh
+++ setup_vim.sh
@@ -4,6 +4,8 @@ mydir=`dirname $mypath`
 apt update
 apt install vim -y
 cp $mydir/.vimrc /root/.vimrc
+mkdir -p ~/.config/nvim
+ln -s /root/.vimrc ~/.config/nvim/init.vim
 #echo "set nu" >> /root/.vimrc
 #echo "set tabstop=4" >> /root/.vimrc
 #echo "set shiftwidth=4" >> /root/.vimrc
